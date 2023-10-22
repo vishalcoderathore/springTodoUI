1a5c84e3133be2cf85937f1a17c25d6cc7bf81e8
Vishal Rathore <vshl.rathore87@gmail.com>
2023-10-21 22:21:38 -0500

    Added some padding in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,20 +9,23 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ isLoggedIn, onLogout }) => {
   const navigate = useNavigate();
 
+  const handleButtonClick = (): void => {
+    if (isLoggedIn) {
+      onLogout();
+    } else {
+      navigate('/login');
+    }
+  };
+
   return (
     <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand mb-0 h1">Todo Spring Boot</span>
-      <button
-        className="btn btn-primary"
-        onClick={(): void => {
-          if (isLoggedIn) {
-            onLogout();
-          } else {
-            navigate('/login');
-          }
-        }}>
-        {isLoggedIn ? 'Logout' : 'Login'}
-      </button>
+      <div className="container">
+        {/* Added me-auto class to push the button to the right */}
+        <span className="navbar-brand mb-0 h1 me-auto">Todo Spring Boot</span>
+        <button className="btn btn-primary" onClick={handleButtonClick}>
+          {isLoggedIn ? 'Logout' : 'Login'}
+        </button>
+      </div>
     </nav>
   );
 };
